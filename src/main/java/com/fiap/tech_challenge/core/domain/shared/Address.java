9b5ff1a8ce317afce1358994d96ba8bf64a274e2
Brenda Souza <breudes@outlook.com>
2025-09-09 11:05:26 -0300

    Adjust Address

--- src/main/java/com/fiap/tech_challenge/core/domain/shared/Address.java
+++ src/main/java/com/fiap/tech_challenge/core/domain/shared/Address.java
@@ -1,4 +1,41 @@
 package com.fiap.tech_challenge.core.domain.shared;
 
 public class Address {
+    private Long id;
+    private String postalCode;
+    private String street;
+    private String additionalDetails;
+    private String city;
+    private String country;
+
+    // All Args Constructor
+    public Address() {
+        if (postalCode == null || postalCode.isBlank()) {
+            throw new IllegalArgumentException("Postal code cannot be empty.");
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getAdditionalDetails() {
+        return additionalDetails;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }
