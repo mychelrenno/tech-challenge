739b45dfd6872a03f686d01b1e8f2b79197c45f3
mychel.renno <mychel.renno@wises.com.br>
2025-09-16 16:15:20 -0300

    feature - atualizado validacao para o atualizar

--- src/main/java/com/fiap/tech_challenge/interfaces/dto/UserTypeDto.java
+++ src/main/java/com/fiap/tech_challenge/interfaces/dto/UserTypeDto.java
@@ -8,6 +8,6 @@ import jakarta.validation.constraints.NotNull;
 public record UserTypeDto(
         @NotNull(message = "the identifier is required", groups = Update.class)
         Long id,
-        @NotBlank(message = "the name is required", groups = Create.class)
+        @NotBlank(message = "the name is required", groups = {Create.class, Update.class})
         String name
 ) {}
