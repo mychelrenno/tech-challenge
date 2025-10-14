7bdd945ccf702bd0e088d4cbe05a9c91086e32ad
Brenda Souza <breudes@outlook.com>
2025-10-14 11:47:44 -0300

    Add owners use cases unit tests

---
+++ src/test/java/com/fiap/tech_challenge/core/usecase/owner/CreateOwnerUseCaseTest.java
@@ -0,0 +1,136 @@
+package com.fiap.tech_challenge.core.usecase.owner;
+
+import com.fiap.tech_challenge.core.domain.Owner;
+import com.fiap.tech_challenge.core.domain.UserType;
+import com.fiap.tech_challenge.core.domain.restaurant.Restaurant;
+import com.fiap.tech_challenge.core.domain.shared.Address;
+import com.fiap.tech_challenge.core.domain.user.User;
+import com.fiap.tech_challenge.core.exception.InvalidAttributeException;
+import com.fiap.tech_challenge.core.repository.OwnerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class CreateOwnerUseCaseTest {
+    private OwnerRepository ownerRepository;
+    private CreateOwnerUseCase createOwnerUseCase;
+
+    @BeforeEach
+    void setUp() {
+        ownerRepository = Mockito.mock(OwnerRepository.class);
+        createOwnerUseCase = new CreateOwnerUseCase(ownerRepository);
+    }
+
+    private Address createDomainAddress() {
+        return new Address("12345", "Main Street", "Suite 1", "New York", "USA");
+    }
+
+    private Restaurant createDomainRestaurant() {
+        return new Restaurant(1L, "Bistro 88", createDomainAddress(), "Italian", "09:00-22:00", null);
+    }
+
+    private UserType createDomainUserType() {
+        return new UserType(1L, "CUSTOMER");
+    }
+
+    private User createDomainUser(){
+        return new User(
+                5L,
+                "Alice",
+                "alice@example.com",
+                "alice123",
+                "secret",
+                createDomainUserType(),
+                createDomainAddress(),
+                new Date(),
+                true
+        );
+    }
+
+    @Test
+    void shouldCreateOwnerSuccessfully() {
+        // given
+        Owner owner = new Owner(
+                null,
+                "123456789",
+                List.of(createDomainRestaurant()),
+                createDomainUser()
+        );
+
+        when(ownerRepository.save(owner)).thenReturn(true);
+
+        // when
+        Boolean result = createOwnerUseCase.execute(owner);
+
+        // then
+        assertTrue(result);
+        verify(ownerRepository, times(1)).save(owner);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenOwnerIsNull() {
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> createOwnerUseCase.execute(null)
+        );
+        assertEquals("Owner cannot be empty.", exception.getMessage());
+        verifyNoInteractions(ownerRepository);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDocumentIsNull() {
+        Owner owner = new Owner(
+                null,
+                null,
+                List.of(createDomainRestaurant()),
+                createDomainUser()
+        );
+
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> createOwnerUseCase.execute(owner)
+        );
+        assertEquals("Owner's document cannot be empty.", exception.getMessage());
+        verifyNoInteractions(ownerRepository);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenRestaurantsIsEmpty() {
+        Owner owner = new Owner(
+                null,
+                "123456789",
+                List.of(),
+                createDomainUser()
+        );
+
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> createOwnerUseCase.execute(owner)
+        );
+        assertEquals("Owner must have a least one restaurant active.", exception.getMessage());
+        verifyNoInteractions(ownerRepository);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUserIsNull() {
+        Owner owner = new Owner(
+                null,
+                "123456789",
+                List.of(createDomainRestaurant()),
+                null
+        );
+
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> createOwnerUseCase.execute(owner)
+        );
+        assertEquals("User's info cannot be empty.", exception.getMessage());
+        verifyNoInteractions(ownerRepository);
+    }
+}
---
+++ src/test/java/com/fiap/tech_challenge/core/usecase/owner/UpdateOwnerUseCaseTest.java
@@ -0,0 +1,183 @@
+package com.fiap.tech_challenge.core.usecase.owner;
+
+import com.fiap.tech_challenge.core.domain.Owner;
+import com.fiap.tech_challenge.core.domain.UserType;
+import com.fiap.tech_challenge.core.domain.restaurant.Restaurant;
+import com.fiap.tech_challenge.core.domain.shared.Address;
+import com.fiap.tech_challenge.core.domain.user.User;
+import com.fiap.tech_challenge.core.exception.InvalidAttributeException;
+import com.fiap.tech_challenge.core.exception.ResourceNotFoundException;
+import com.fiap.tech_challenge.core.repository.OwnerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class UpdateOwnerUseCaseTest {
+    private OwnerRepository ownerRepository;
+    private UpdateOwnerUseCase updateOwnerUseCase;
+
+    @BeforeEach
+    void setUp() {
+        ownerRepository = mock(OwnerRepository.class);
+        updateOwnerUseCase = new UpdateOwnerUseCase(ownerRepository);
+    }
+
+    private Address createDomainAddress() {
+        return new Address("12345", "Main Street", "Suite 1", "New York", "USA");
+    }
+
+    private Restaurant createDomainRestaurant() {
+        return new Restaurant(1L, "Bistro 88", createDomainAddress(), "Italian", "09:00-22:00", null);
+    }
+
+    private UserType createDomainUserType() {
+        return new UserType(1L, "CUSTOMER");
+    }
+
+    private User createDomainUser(){
+        return new User(
+                5L,
+                "Alice",
+                "alice@example.com",
+                "alice123",
+                "secret",
+                createDomainUserType(),
+                createDomainAddress(),
+                new Date(),
+                true
+        );
+    }
+
+    private Owner createValidOwner() {
+        return new Owner(
+                null,
+                "Valid document",
+                List.of(createDomainRestaurant()),
+                createDomainUser()
+        );
+    }
+
+    @Test
+    void shouldUpdateOwnerSuccessfully() {
+        // given
+        Long ownerId = 1L;
+        Owner existingOwner = createValidOwner();
+        Owner updatedData = createValidOwner();
+
+        when(ownerRepository.findById(ownerId)).thenReturn(existingOwner);
+        when(ownerRepository.update(ownerId, updatedData)).thenReturn(true);
+
+        // when
+        boolean result = updateOwnerUseCase.execute(ownerId, updatedData);
+
+        // then
+        assertTrue(result);
+        verify(ownerRepository, times(1)).findById(ownerId);
+        verify(ownerRepository, times(1)).update(ownerId, updatedData);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenOwnerNotFound() {
+        // given
+        Long ownerId = 999L;
+        Owner updatedData = createValidOwner();
+
+        when(ownerRepository.findById(ownerId)).thenReturn(null);
+
+        // when / then
+        ResourceNotFoundException exception = assertThrows(
+                ResourceNotFoundException.class,
+                () -> updateOwnerUseCase.execute(ownerId, updatedData)
+        );
+
+        assertEquals("Owner not found.", exception.getMessage());
+        verify(ownerRepository, times(1)).findById(ownerId);
+        verify(ownerRepository, never()).update(anyLong(), any());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUpdatedOwnerIsNull() {
+        Long ownerId = 1L;
+        when(ownerRepository.findById(ownerId)).thenReturn(createValidOwner());
+
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> updateOwnerUseCase.execute(ownerId, null)
+        );
+
+        assertEquals("Owner cannot be empty.", exception.getMessage());
+        verify(ownerRepository, times(1)).findById(ownerId);
+        verify(ownerRepository, never()).update(anyLong(), any());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDocumentIsEmpty() {
+        Long ownerId = 1L;
+        Owner validOwner = createValidOwner();
+        Owner invalidOwner = new Owner(
+                validOwner.getId(),
+                "",
+                validOwner.getRestaurants(),
+                validOwner.getUser()
+        );
+
+        when(ownerRepository.findById(ownerId)).thenReturn(createValidOwner());
+
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> updateOwnerUseCase.execute(ownerId, invalidOwner)
+        );
+
+        assertEquals("Owner's document cannot be empty.", exception.getMessage());
+        verify(ownerRepository, never()).update(anyLong(), any());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenRestaurantsIsEmpty() {
+        Long ownerId = 1L;
+        Owner validOwner = createValidOwner();
+        Owner invalidOwner = new Owner(
+                validOwner.getId(),
+                validOwner.getDocument(),
+                List.of(),
+                validOwner.getUser()
+        );
+
+        when(ownerRepository.findById(ownerId)).thenReturn(createValidOwner());
+
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> updateOwnerUseCase.execute(ownerId, invalidOwner)
+        );
+
+        assertEquals("Owner must have a least one restaurant.", exception.getMessage());
+        verify(ownerRepository, never()).update(anyLong(), any());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUserIsNull() {
+        Long ownerId = 1L;
+        Owner validOwner = createValidOwner();
+        Owner invalidOwner = new Owner(
+                validOwner.getId(),
+                validOwner.getDocument(),
+                validOwner.getRestaurants(),
+                null
+        );
+
+        when(ownerRepository.findById(ownerId)).thenReturn(createValidOwner());
+
+        InvalidAttributeException exception = assertThrows(
+                InvalidAttributeException.class,
+                () -> updateOwnerUseCase.execute(ownerId, invalidOwner)
+        );
+
+        assertEquals("User's info cannot be empty.", exception.getMessage());
+        verify(ownerRepository, never()).update(anyLong(), any());
+    }
+}
